edad8093f171a7632b2c489ed695deeeae1bc1aa
Kamilia Latifah <kamilia.latifah@gmail.com>
2019-05-16 10:19:06 +0700

    Initial Commit

---
+++ UIText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIText
+//
+//  Created by Kamilia Latifah on 16/05/19.
+//  Copyright © 2019 masaksendiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
